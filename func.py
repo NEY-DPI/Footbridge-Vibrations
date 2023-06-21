70c65ef122dfde42b611988f990287f61419c895
Denis Piron <dpi@ney.partners>
2023-06-21 09:36:46 +0200

    Commit

---
+++ func.py
@@ -0,0 +1,263 @@
+import pandas as pd
+import math as m
+
+
+def get_freq(file):
+    df = pd.read_csv(file)
+    df.columns = ['LC', 'Title', 'f']
+    return df
+
+
+def check_freq(df_in):
+    df = df_in
+    f_min = df_in.f.min()
+    if f_min > 5:
+        df['vert_to_check'] = False
+    else:
+        vert = (1.7 < df.f) & (df.f < 2.2)
+        df['vert_to_check'] = vert
+
+    hor = (0.3 < df.f) & (df.f < 2.5)
+    df['hor_to_check'] = hor
+
+    return df
+
+
+def get_nodes(file):
+    df = pd.read_csv(file)
+    df.columns = ['NODE', 'X', 'Y', 'Z']
+    return df
+
+
+def get_Si(file):
+    df = pd.read_csv(file)
+    df.columns = ['NODE', 'Si']
+    df['Si'] = df['Si'].astype(float)
+    df['Si'] = abs(df['Si'])
+    return df
+
+
+def merge_nodes(nodes, Si):
+    df = pd.merge(nodes, Si, on='NODE')
+    return df
+
+
+def get_modal_disp(file):
+    df = pd.read_csv(file, low_memory=False)
+    df.columns = ['LC', 'Title', 'NODE', 'ux', 'uy', 'uz']
+    return df
+
+
+def scale_modal_disp(modal_disp, factor):
+    df = modal_disp
+    df['sc_ux'] = modal_disp['ux'] * factor
+    df['sc_uy'] = modal_disp['uy'] * factor
+    df['sc_uz'] = modal_disp['uz'] * factor
+    return df
+
+
+def get_axis(dir, vertical, longitudinal, lateral):
+    if vertical == 'x':
+        vertical = 'sc_ux'
+    elif vertical == 'y':
+        vertical = 'sc_uy'
+    elif vertical == 'z':
+        vertical = 'sc_uz'
+    if longitudinal == 'x':
+        longitudinal = 'sc_ux'
+    elif longitudinal == 'y':
+        longitudinal = 'sc_uy'
+    elif longitudinal == 'z':
+        longitudinal = 'sc_uz'
+    if lateral == 'x':
+        lateral = 'sc_ux'
+    elif lateral == 'y':
+        lateral = 'sc_uy'
+    elif lateral == 'z':
+        lateral = 'sc_uz'
+
+    if dir == 'vertical':
+        return vertical
+    elif dir == 'longitudinal':
+        return longitudinal
+    elif dir == 'lateral':
+        return lateral
+
+
+def get_d(TC, S):
+    if TC == 1:
+        d = 15 / S
+    elif TC == 2:
+        d = 0.2
+    elif TC == 3:
+        d = 0.5
+    elif TC == 4:
+        d = 1
+    return d
+
+
+def get_modal_mass(disp, axis):
+    max_disp_all = max(abs(disp[axis].max()),
+                       abs(disp[axis].min())
+                       )
+    modal_mass = 1 / (max_disp_all ** 2)
+    return modal_mass
+
+
+def get_damping(TC, bridge_type):
+    values = pd.DataFrame(
+        {'type': ['Reinforced Concrete',
+                  'Prestressed Concrete',
+                  'Steel-Concrete',
+                  'Steel',
+                  'Wood'],
+         '<1p/m2': [0.008, 0.005, 0.003, 0.002, 0.010],
+         '>=1p/m2': [0.016, 0.010, 0.006, 0.004, 0.020]
+         })
+    if TC == 4:
+        col = '>=1p/m2'
+    else:
+        col = '<1p/m2'
+    return values[values.type == bridge_type][col].item()
+
+
+def get_N_eq(N, damping, d):
+    if d < 1:
+        return 10.8 * m.sqrt(damping * N)
+    else:
+        return 1.85 * m.sqrt(N)
+
+
+def get_gp(f, direction, phase='design'):
+    if direction == 'vertical':
+        if phase == 'design':
+            if f <= 0.5:
+                gp = 0
+            elif f <= 1.3:
+                gp = 0 + (280 - 0) * (f - 0.5) / (1.3 - 0.5)
+            elif f <= 2.4:
+                gp = 280
+            elif f <= 3:
+                gp = 280 + (70 - 280) * (f - 2.4) / (3 - 2.4)
+            elif f <= 4.8:
+                gp = 70
+            elif f <= 6:
+                gp = 70 + (0 - 70) * (f - 4.8) / (6 - 4.8)
+            else:
+                gp = 0
+        else:
+            if f <= 1:
+                gp = 0
+            elif f <= 1.6:
+                gp = 0 + (280 - 0) * (f - 1) / (1.6 - 1)
+            elif f <= 2.1:
+                gp = 280
+            elif f <= 2.55:
+                gp = 280 + (35 - 280) * (f - 2.1) / (2.55 - 2.1)
+            elif f <= 2.95:
+                gp = 35
+            elif f <= 3.4:
+                gp = 35 + (70 - 35) * (f - 2.95) / (3.4 - 2.95)
+            elif f <= 4.25:
+                gp = 70
+            elif f <= 5:
+                gp = 70 + (0 - 70) * (f - 4.25) / (5 - 4.25)
+            else:
+                gp = 0
+    if direction == 'longitudinal':
+        if phase == 'design':
+            if f <= 0.5:
+                gp = 0
+            elif f <= 1.3:
+                gp = 0 + (140 - 0) * (f - 0.5) / (1.3 - 0.5)
+            elif f <= 2.4:
+                gp = 140
+            elif f <= 3:
+                gp = 140 + (35 - 140) * (f - 2.4) / (3 - 2.4)
+            elif f <= 4.8:
+                gp = 35
+            elif f <= 6:
+                gp = 35 + (0 - 35) * (f - 4.8) / (6 - 4.8)
+            else:
+                gp = 0
+        else:
+            if f <= 1:
+                gp = 0
+            elif f <= 1.6:
+                gp = 0 + (140 - 0) * (f - 1) / (1.6 - 1)
+            elif f <= 2.1:
+                gp = 140
+            elif f <= 2.55:
+                gp = 140 + (17.5 - 140) * (f - 2.1) / (2.55 - 2.1)
+            elif f <= 2.95:
+                gp = 17.5
+            elif f <= 3.4:
+                gp = 17.5 + (35 - 17.5) * (f - 2.95) / (3.4 - 2.95)
+            elif f <= 4.25:
+                gp = 35
+            elif f <= 5:
+                gp = 35 + (0 - 35) * (f - 4.25) / (5 - 4.25)
+            else:
+                gp = 0
+    if direction == 'lateral':
+        if phase == 'design':
+            if f <= 0.15:
+                gp = 0
+            elif f <= 0.4:
+                gp = 0 + (35 - 0) * (f - 0.15) / (0.4 - 0.15)
+            elif f <= 1.2:
+                gp = 35
+            elif f <= 1.4:
+                gp = 35 + (7 - 35) * (f - 1.2) / (1.4 - 1.2)
+            elif f <= 2.4:
+                gp = 7
+            elif f <= 2.8:
+                gp = 7 + (0 - 7) * (f - 2.4) / (2.8 - 2.4)
+            else:
+                gp = 0
+        else:
+            if f <= 0.3:
+                gp = 0
+            elif f <= 0.5:
+                gp = 0 + (35 - 0) * (f - 0.3) / (0.5 - 0.3)
+            elif f <= 1.1:
+                gp = 35
+            elif f <= 1.3:
+                gp = 35 + (3.5 - 35) * (f - 1.1) / (1.3 - 1.1)
+            elif f <= 1.45:
+                gp = 3.5
+            elif f <= 1.7:
+                gp = 3.5 + (7 - 3.5) * (f - 1.45) / (1.7 - 1.45)
+            elif f <= 2.1:
+                gp = 7
+            elif f <= 2.5:
+                gp = 7 + (0 - 7) * (f - 2.1) / (2.5 - 2.1)
+            else:
+                gp = 0
+    return gp
+
+
+def merge_all_mode(displ, nodes):
+    df = pd.merge(nodes, displ, on='NODE')
+    return df
+
+
+def get_a_req(direction, TC, traffic):
+    if traffic == 'Dense':
+        values = pd.DataFrame(
+            {'TC': [1, 2, 3, 4],
+             'vertical': [0.70, 1, 1, 2.5],
+             'longitudinal': [0.2, 0.3, 0.3, 0.4],
+             'lateral': [0.2, 0.3, 0.3, 0.4]
+            }
+        )
+    else:
+        values = pd.DataFrame(
+            {'TC': [1, 2, 3, 4],
+             'vertical': [0.70, 1, 2.5, 2.5],
+             'longitudinal': [0.2, 0.3, 0.4, 0.4],
+             'lateral': [0.2, 0.3, 0.4, 0.4]
+            }
+        )
+    return values[values.TC == TC][direction].item()
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
 import streamlit as st
 import pandas as pd
+import func as f
+import math as m
 from io import StringIO
 
 '''
@@ -11,7 +12,167 @@ from io import StringIO
 
 "## Import Files"
 "### Eigenfrequencies"
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+eigenfrequencies = st.file_uploader("Import Eigenfrequencies", key='eigen')
+if eigenfrequencies is not None:
+    freq = f.get_freq(eigenfrequencies)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        first_lc_empty = st.number_input('First LC of eigenfrequency - empty bridge:', value=7500, step=1)
+    with col2:
+        first_lc_loaded = st.number_input('First LC of eigenfrequency - loaded bridge:', value=7600, step=1)
+
+    freq_empty = freq[(freq['LC'] < first_lc_loaded)].copy().reset_index()
+    freq_loaded = freq[(freq['LC'] >= first_lc_loaded)].copy().reset_index()
+    checked_freq = f.check_freq(freq_empty)
+    freq_to_calc = checked_freq[(checked_freq.vert_to_check == True) | (checked_freq.hor_to_check == True)]
+    st.write('ACCELERATIONS NEED TO BE CHECKED FOR THE FOLLOWING MODES:')
+    freq_to_calc
+
+
+    modal_displacements = st.file_uploader("Import modal displacements", key='modal_disp')
+    node_coordinates = st.file_uploader("Import node coordinates", key='node_coord')
+    unit_load_Si = st.file_uploader("Import unit load Si", key='Si')
+    if modal_displacements is not None and node_coordinates is not None and unit_load_Si is not None:
+        col1, col2 = st.columns(2)
+        with col1:
+            software_scaling_factor = st.selectbox('Software scaling factor', ['Sofistik'], key='software')
+            bridge_type = st.selectbox('Bridge Type', [
+                'Steel',
+                'Steel-Concrete',
+                'Reinforced Concrete',
+                'Prestressed Concrete',
+                'Wood'], key='bridge')
+            traffic = st.selectbox('Traffic Type', ['Dense', 'Not Dense'], key='traffic')
+        with col2:
+            vertical = st.selectbox('Vertical axis', ['x', 'y', 'z'], index=2, key='vert')
+            longitudinal = st.selectbox('Longitudinal axis', ['x', 'y', 'z'], index=0, key='long')
+            lateral = st.selectbox('Lateral axis', ['x', 'y', 'z'], index=1, key='lat')
+
+        # Calculation
+        if software_scaling_factor == "Sofistik":
+            factor_scaling = 0.001 / m.sqrt(1000)  # Scaling factor for Sofistik
+
+        nodes = f.get_nodes(node_coordinates)
+        Si = f.get_Si(unit_load_Si)
+        nodes = f.merge_nodes(nodes, Si)
+
+        modal_disp = f.get_modal_disp(modal_displacements)
+        modal_disp = f.scale_modal_disp(modal_disp, factor_scaling)
+        modal_disp_empty = modal_disp[(modal_disp['LC'] < first_lc_loaded)].copy().reset_index()
+        modal_disp_loaded = modal_disp[(modal_disp['LC'] >= first_lc_loaded)].copy().reset_index()
+        S_total = nodes.Si.sum()
+
+        n_lc = freq_to_calc['LC'].count()
+
+        results_mode = []
+        for mode_id in freq_to_calc.index.array:
+            # Check mode
+            mode = mode_id + 1
+            mode_lc_empty = first_lc_empty + mode_id
+            mode_lc_loaded = first_lc_loaded + mode_id
+            mode_f_empty = freq_empty[freq_empty.LC == mode_lc_empty]['f'].item()
+            mode_f_loaded = freq_loaded[freq_loaded.LC == mode_lc_loaded]['f'].item()
+            # print(f'CHECK MODE {mode} - freq = {mode_f_empty} (empty), {mode_f_loaded} (loaded) :')
+            results = pd.DataFrame(columns=['vert1', 'vert2', 'vert3', 'vert4', 'vert5', 'long1', 'long2', 'long3', 'long4', 'long5', 'lat1', 'lat2', 'lat3', 'lat4', 'lat5'])
+            results.loc[0] = ['', 'TC 1', 'TC 2', 'TC 3', 'TC 4', '', 'TC 1', 'TC 2', 'TC 3', 'TC 4', '', 'TC 1',
+                              'TC 2', 'TC 3', 'TC 4']
+            for i in range(1, 15):
+                results.loc[i] = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+            results.iloc[1:, 0] = ['d (p/m2)',
+                                   'N (-)',
+                                   'N_lim (-)',
+                                   '<5% modal mass?',
+                                   'Modal mass',
+                                   'Load',
+                                   'Damping (%)',
+                                   'Neq (-)',
+                                   'gamma P (N)',
+                                   'q_eq',
+                                   'a_max',
+                                   'a_lim',
+                                   'is acceptable?',
+                                   'm_tmd'
+                                   ]
+
+            index = 1
+            for dir in ['vertical', 'longitudinal', 'lateral']:
+                axis = f.get_axis(dir, vertical, longitudinal, lateral)
+                for TC in range(1, 5):
+                    d = f.get_d(TC, S_total)
+                    N = d * S_total
+
+                    disp = modal_disp_empty[modal_disp_empty.LC == mode_lc_empty]
+                    # Check if pedestrian loads need to be considered
+                    mj = f.get_modal_mass(disp, axis)
+                    N_limit = 0.05 * mj / 70
+
+                    if N > N_limit:
+                        is_less_5pc = False
+                        load = 'loaded'
+                        disp = modal_disp_loaded[modal_disp_loaded.LC == mode_lc_loaded]
+                        mode_f = mode_f_loaded
+                    else:
+                        is_less_5pc = True
+                        load = 'empty'
+                        mode_f = mode_f_empty
+
+                    damping = f.get_damping(TC, bridge_type)
+                    N_eq = f.get_N_eq(N, damping, d)
+                    gp = f.get_gp(mode_f, dir)
+                    q_eq = N_eq / S_total * gp
+
+                    table_mode = f.merge_all_mode(disp, nodes)
+                    sum_si_disp_df = abs(table_mode[axis]) * table_mode['Si']
+                    sum_si_disp = sum_si_disp_df.sum()
+                    max_disp = abs(table_mode[axis]).max()
+                    a_max = q_eq * sum_si_disp / (2 * damping) * max_disp
+
+                    a_req = f.get_a_req(dir, TC, traffic)
+                    is_ok = a_max < a_req
+
+                    if is_ok:
+                        m_tmd = '-'
+                    else:
+                        a_new = a_max
+                        damping_new = damping
+                        while a_new > a_req:
+                            damping_new += 0.00001
+                            N_eq_new = f.get_N_eq(N, damping_new, d)
+                            q_eq_new = N_eq_new / S_total * gp
+                            a_new = q_eq_new * sum_si_disp / (2 * damping_new) * max_disp
+                        damping_eff = damping
+                        m_tmd = 0
+                        while damping_eff < damping_new:
+                            m_tmd += 1
+                            mu = m_tmd / mj
+                            damping_eff = damping + (2 * m.sqrt(2 / (mu * (1 + mu)))) ** -1
+
+                    results.iloc[1:, index] = [d, N, N_limit, is_less_5pc, mj, load, damping * 100, N_eq, gp, q_eq,
+                                               a_max, a_req, is_ok, m_tmd]
+
+                    index += 1
+                index += 1
+
+            results_mode.append(results)
+            # print(results[:][:])
+            # print('')
+
+        mode_list = freq_to_calc.index.array + 1
+        mode_list_str = [str(x) for x in mode_list]
+        mose_list_int = [int(x) for x in mode_list]
+
+        mode = st.selectbox('Select mode', mode_list_str, index=0, key='mode')
+        mode_id = int(mode) - 1
+        mode_lc_empty = first_lc_empty + mode_id
+        mode_lc_loaded = first_lc_loaded + mode_id
+        mode_f_empty = freq_empty[freq_empty.LC == mode_lc_empty]['f'].item()
+        mode_f_loaded = freq_loaded[freq_loaded.LC == mode_lc_loaded]['f'].item()
+        st.write(f'CHECK MODE {mode} - freq = {mode_f_empty} (empty), {mode_f_loaded} (loaded) :')
+        st.dataframe(results_mode[mode_id],
+                     column_config={
+                         "d (p/m2)": st.column_config.NumberColumn(format="{:.2f}")
+                     },
+                     hide_index=True)
+
+
